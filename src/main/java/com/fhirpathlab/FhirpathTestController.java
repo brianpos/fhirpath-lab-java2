595cab9604fc5157a0a99143ee263672384f05ba
Brian Postlethwaite <brian_pos@hotmail.com>
2025-04-11 14:05:14 +1000

    Update to org.hl7.fhir v6.5.18

--- src/main/java/com/fhirpathlab/FhirpathTestController.java
+++ src/main/java/com/fhirpathlab/FhirpathTestController.java
@@ -214,7 +214,7 @@ public class FhirpathTestController {
         var responseParameters = new Parameters();
         responseParameters.setId("fhirpath");
         var paramsPart = ParamUtils.add(responseParameters, "parameters");
-        ParamUtils.add(paramsPart, "evaluator", "Java 6.4.0 (r4b)");
+        ParamUtils.add(paramsPart, "evaluator", "Java 6.5.18 (r4b)");
         ParamUtils.add(paramsPart, "context", contextExpression);
         ParamUtils.add(paramsPart, "expression", expression);
 
